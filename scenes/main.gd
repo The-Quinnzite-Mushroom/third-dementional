1d3754e92f3ed517b59368890532ebd59994c189
The-Quinnzite-Mushroom <quinn@trusler.net>
2025-10-03 19:16:39 -0700

    removed project.godot

---
+++ scenes/main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+var num = 5
+
+
+func my_cool_function():
+	print("hello world")
+	
+	
+func _process(delta: float) -> void:
+	print("hellow world")
+	print("this is delta",delta)
