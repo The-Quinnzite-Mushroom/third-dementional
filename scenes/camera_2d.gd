28d25e1997529e771b7747049e43a84f7c6ac35f
gnorris55 <graemenorris99@gmail.com>
2025-10-05 16:15:20 -0700

    added slots for classes of the player

--- scenes/camera_2d.gd
+++ scenes/camera_2d.gd
@@ -9,4 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position = target.global_positions
+	global_position = target.global_position
